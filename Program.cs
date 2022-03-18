fba78c09e26528ad6019a5c5c4f3329240b5d6b0
Liubov K <lyouso@yandex.ru>
2022-03-18 16:21:45 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+MakeReverse(number);
+
+void MakeReverse(int current_number)
+{
+    int ch1 = current_number / 100;
+    int ch2 = (current_number / 10) % 10;
+    int ch3 = (current_number % 100) % 10;
+    Console.WriteLine(current_number + " >> " + $"{ch3}" + $"{ch2}" + $"{ch1}");
+}
