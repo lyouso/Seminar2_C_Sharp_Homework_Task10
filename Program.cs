8910e9a6cfa1c74612d7b85b56d1b854d89b67f6
Liubov K <lyouso@yandex.ru>
2022-03-18 16:35:09 +0300

    Added few changes_1

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ void MakeReverse(int current_number)
                 int ch1 = current_number / 100;
                 int ch2 = (current_number / 10) % 10;
                 int ch3 = (current_number % 100) % 10;
-            Console.WriteLine(current_number + " >> " + $"{ch3}" + $"{ch2}" + $"{ch1}");
+                Console.WriteLine(current_number + " >> " + $"{ch3}" + $"{ch2}" + $"{ch1}");
             }
     }
 
