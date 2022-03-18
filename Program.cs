35062055640fc179c36c0ffe4d19231ea0c6317a
Liubov K <lyouso@yandex.ru>
2022-03-18 16:34:29 +0300

    Added few changes

--- Program.cs
+++ Program.cs
@@ -4,8 +4,23 @@ MakeReverse(number);
 
 void MakeReverse(int current_number)
 {
-    int ch1 = current_number / 100;
-    int ch2 = (current_number / 10) % 10;
-    int ch3 = (current_number % 100) % 10;
-    Console.WriteLine(current_number + " >> " + $"{ch3}" + $"{ch2}" + $"{ch1}");
+    if (current_number < 100) 
+    {
+        Console.WriteLine("Вы ввели неверное число, введите заново");
+    }
+    else
+    {
+        if (current_number > 1000)
+        {
+            Console.WriteLine("Вы ввели неверное число, введите заново");
+        }
+        else 
+            {
+                int ch1 = current_number / 100;
+                int ch2 = (current_number / 10) % 10;
+                int ch3 = (current_number % 100) % 10;
+            Console.WriteLine(current_number + " >> " + $"{ch3}" + $"{ch2}" + $"{ch1}");
+            }
+    }
+
 }
